21291c484bc12464c8d24591db46fc9f4372df29
qjlightwala <quaidjoharlightwala901@gmail.com>
2026-08-18 11:24:29 +0530

    done

--- src/main/java/in/co/rays/proj4/util/JDBCDataSource.java
+++ src/main/java/in/co/rays/proj4/util/JDBCDataSource.java
@@ -1,5 +1,81 @@
 package in.co.rays.proj4.util;
 
-public class JDBCDataSource {
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public final class JDBCDataSource {
+
+	private static JDBCDataSource jdbc = null;
+	private static ComboPooledDataSource cpds = null;
+	private static ResourceBundle rb = ResourceBundle.getBundle("com.rays.budle.system");
+
+	private JDBCDataSource() {
+		cpds = new ComboPooledDataSource();
+
+		try {
+
+			cpds.setDriverClass(rb.getString("driver"));
+			cpds.setJdbcUrl(rb.getString("url"));
+			cpds.setUser(rb.getString("username"));
+			cpds.setPassword(rb.getString("password"));
+			cpds.setMaxPoolSize(30);
+			cpds.setMinPoolSize(5);
+			cpds.setAcquireIncrement(5);
+			cpds.setInitialPoolSize(5);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static JDBCDataSource getInstance() {
+		if (jdbc == null) {
+			jdbc = new JDBCDataSource();
+			return jdbc;
+		}
+		return null;
+
+	}
+
+	public static Connection getConnection() {
+		try {
+
+			return getInstance().cpds.getConnection();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		}
+
+		return null;
+	}
+
+	public static void closeConnection(Connection conn) {
+		if (conn != null) {
+			try {
+				conn.close();
+			}
+
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public static void trnRollBack(Connection conn) {
+
+		if (conn != null) {
+			try {
+				conn.rollback();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
 
 }
